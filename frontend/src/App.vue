a510aa3d8a82e0cb8fc75c3fc24d0eb736fa4328
hpgcpy <hongsun724@gmail.com>
2026-07-15 09:14:52 +0900

    App.vue 수정

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,6 @@
 <script setup>
 import { ref } from 'vue'
-
 import ChatBotButton from './components/chatbot/ChatBotButton.vue'
-
 import ChatBotWindow from './components/chatbot/ChatBotWindow.vue'
 
 const isOpen = ref(false)
@@ -10,17 +8,17 @@ const isOpen = ref(false)
 
 <template>
   <div>
-    <h1>게시판 페이지</h1>
-
-    <p>게시판 영역</p>
-
+    <header>
+      <h1>게시판 페이지</h1>
+      <p>게시판 영역</p>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/map">Map</RouterLink>
       </nav>
-  </header>
-  <ChatBotWindow v-if="isOpen" />
+    </header>
+
+    <ChatBotWindow v-if="isOpen" />
     <ChatBotButton @toggle="isOpen = !isOpen" />
   </div>
 </template>
